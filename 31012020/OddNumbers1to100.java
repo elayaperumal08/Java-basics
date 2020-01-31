69a9646c1ff6f94bae71375e377923cec644a02f
elayaperumal <elayaperumalee09@gmail.com>
2020-01-31 13:06:32 +0530

    OddNumbers1to100.java

---
+++ 31012020/OddNumbers1to100.java
@@ -0,0 +1,9 @@
+//Displayevennumbersfrom1to100
+public class OddNumbers1to100 {
+	public static void main(String[] args) {
+        int n = 100;
+for (int i = 1; i<=n; i++){
+if ( i %2 == 1 )
+System.out.println(i);
+}}}
+
