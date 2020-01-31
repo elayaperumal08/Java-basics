c7509ddbe1451f76cb9240d5fe136e183fa0a1cd
elayaperumal <elayaperumalee09@gmail.com>
2020-01-31 13:13:20 +0530

    using not equal to oprator

--- 31012020/OddNumbers1to100.java
+++ 31012020/OddNumbers1to100.java
@@ -3,7 +3,7 @@ public class OddNumbers1to100 {
 	public static void main(String[] args) {
         int n = 100;
 for (int i = 1; i<=n; i++){
-if ( i %2 == 1 )
+if ( i %2 !=0 )
 System.out.println(i);
 }}}
 
