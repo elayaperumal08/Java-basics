81929e28a3368e8421cbbfc5211b82b215757b4b
elayaperumal <elayaperumalee09@gmail.com>
2020-02-05 18:53:15 +0530

    Adding IntegerDataTypes

---
+++ 04022020/IntegerDataTypes.java
@@ -0,0 +1,19 @@
+class IntegerDataTypes {
+	public static void main (String[] arguments) {
+short a = 1000;
+char charVar = 'E';
+int i = 1;
+long s = 32000;
+float m = 1.3214f;
+byte b = 125;
+double d = 5.2244;
+
+		System.out.println("short: "+ a);
+System.out.println("char: "+ charVar);
+System.out.println("int: "+ i);
+System.out.println("long: "+ s);
+System.out.println("float: "+ m);
+System.out.println("byte: "+ b);
+System.out.println("double: "+ d);
+		}
+}
