ff4dde4bcf60e9269aabdb8b13ed6249baf756eb
elayaperumal <elayaperumalee09@gmail.com>
2020-02-06 17:44:43 +0530

    StringMethods

--- 06022020/StringMethods.java
+++ 06022020/StringMethods.java
@@ -7,5 +7,10 @@ String s3 = "ElayaPerumal";
 System.out.println("length of String s3: " + s3.length());
 System.out.println("charactor at position 5: " + s3.charAt(5));
 System.out.println("index of charactor 'u': " + s3.indexOf('u'));
+System.out.println("contains sequenc 'mal' : " + s3.contains("mal"));
+System.out.println("endswith character 'm' : " + s3.endsWith("m"));
+System.out.println("replace 'Elaya' with 'Small' : " + s3.replace("Elaya", "Small"));
+System.out.println("Convert to LowerCase : " + s3.toLowerCase());
+System.out.println("Convert to upperCase: " + s3.toUpperCase()); 
 }
   } 
