72f18fb4178e3eae9e0536df083dfa50d01c0d37
elayaperumal <elayaperumalee09@gmail.com>
2020-02-06 18:47:38 +0530

    ContainsMethodifalseConditions.java

---
+++ 06022020/ContainsMethodifalseConditions.java
@@ -0,0 +1,13 @@
+class ContainsMethodifalseConditions {
+ public static void main(String args[]) {
+  String s1 = "Elayaperumal is good boy and he has good family";
+if (s1.contains("super"))
+{
+System.out.println("The keyword is super : is there in given string");
+}
+else 
+{
+System.out.println("The keyword is super : is not there in s1");
+}
+  } 
+}
