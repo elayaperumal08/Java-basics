9e825ed2e915f4a89ddb8ea45611043603cc5c60
elayaperumal <elayaperumalee09@gmail.com>
2020-02-06 13:42:14 +0530

    StringMethods

---
+++ 06022020/StringMethods.java
@@ -0,0 +1,11 @@
+class StringMethods {
+ public static void main(String args[]) {
+  String s1 = "Elaya";
+String s2 = "Perumal";
+System.out.println("String s3 = " + s1.concat(s2));
+String s3 = "ElayaPerumal";
+System.out.println("length of String s3: " + s3.length());
+System.out.println("charactor at position 5: " + s3.charAt(5));
+System.out.println("index of charactor 'u': " + s3.indexOf('u'));
+}
+  } 
