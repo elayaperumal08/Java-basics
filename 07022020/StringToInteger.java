525df28de9c62a0da1903535fa01b9b51f2b2c1e
elayaperumal <elayaperumalee09@gmail.com>
2020-02-07 19:15:30 +0530

    StringToInterBothMethod

---
+++ 07022020/StringToInteger.java
@@ -0,0 +1,11 @@
+public class StringToInteger {
+    public static void main(String args[]) {
+        String number = "1989";
+int result = Integer.parseInt(number);
+
+        System.out.println("Convert into Int: " + result);
+String sretest = "2020";
+int elaya = Integer.valueOf(sretest);
+System.out.println("Convert into Int using valueOf: " + elaya);
+            }
+}
