565ccf85bf3b4135b34efb455439c5822fdf9674
elayaperumal <elayaperumalee09@gmail.com>
2020-02-12 20:27:29 +0530

    EmployeeMain.java

---
+++ 12022020/EmployeeMain.java
@@ -0,0 +1,18 @@
+class EmployeeMain {
+		
+	public static void main(String args[]) {
+
+	Employee elaya = new Employee ("Elayaperumal", 30000);
+	System.out.println(elaya.name);
+        System.out.println(elaya.salary);
+System.out.println(elaya.getsalary());
+        Employee jegs = new Employee ("jegatheesh", 100000);
+        System.out.println(jegs.name);
+        System.out.println(jegs.salary);
+System.out.println(jegs.getsalary());
+
+
+
+	}
+}
+
