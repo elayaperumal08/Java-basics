77bc0787c14a16881735edf098469c5fc85e8bc1
elayaperumal <elayaperumalee09@gmail.com>
2020-02-12 20:26:13 +0530

    EmployeeStatic

---
+++ 12022020/Employee.java
@@ -0,0 +1,20 @@
+class Employee {
+	
+
+	// instance variables
+	String name;
+	int salary;
+        
+	
+	Employee(String name, int salary) {
+		this.name = name;
+		this.salary = salary;
+} String getsalary(){
+return "Salary is :" +this.salary;
+}
+	String totalSalary(){
+return "TotalSalary :" +EmployeeMain;
+		
+}
+String TotalSalary(){
+
